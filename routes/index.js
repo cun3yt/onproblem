9117bacd2afdc9a0cc79c5b6ce54067658eb8850
Cuneyt Mertayak <cuneyt@ceng.metu.edu.tr>
2017-01-17 22:14:26 +0300

    Removal of Debug Messages

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-
-  var debug = require('debug')('onproblem:server');
-  debug(models);
-
   models.Problem.findAll().then(function(problems){
-
-    debug('hello');
-    debug(models);
-    debug(problems);
-
     res.render('index', {
       title: 'OnProblem',
       problems: problems
