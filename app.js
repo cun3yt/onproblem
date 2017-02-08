e3a80982c2e529122462fb231c39b352dabbd079
Cuneyt Mertayak <cuneyt@ceng.metu.edu.tr>
2017-02-08 15:06:29 +0300

    Separate File for Error Middlewares

--- app.js
+++ app.js
@@ -39,24 +39,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Routes Configuration
-require('./controllers/mapping')(app, passport, debug);
+require('./controllers/mapping')(app, passport);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// Error Middleware(s)
+require('./config/error-middleware')(app);
 
 module.exports = app;
