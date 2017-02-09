8838333107c82996f1cdfc7b074981b9411daae3
Cuneyt Mertayak <cuneyt@ceng.metu.edu.tr>
2017-02-09 18:20:05 +0300

    Fix for Deprecation Warning

--- config/request-parser.js
+++ config/request-parser.js
@@ -7,4 +7,9 @@ module.exports = function(app) {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(cookieParser());
+
+  // based on this Q&A: http://bit.ly/2kXgNGj
+  if(process.env["DEBUG_FD"]) {
+    delete process.env["DEBUG_FD"];
+  }
 };
