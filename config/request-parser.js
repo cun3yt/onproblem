868ad93457c1fd7f09268d3daefc2812a42599fc
Cuneyt Mertayak <cuneyt@ceng.metu.edu.tr>
2017-02-13 23:45:03 +0300

    Timeout Middleware

--- config/request-parser.js
+++ config/request-parser.js
@@ -2,11 +2,18 @@ module.exports = function(app) {
   var logger = require('morgan');
   var bodyParser = require('body-parser');
   var cookieParser = require('cookie-parser');
+  var timeout = require('connect-timeout');
 
   app.use(logger('dev'));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(cookieParser());
+  app.use(timeout('3s', {}));
+  app.use(haltOnTimeout);
+
+  function haltOnTimeout(req, res, next) {
+    if(!req.timedout) { next(); }
+  }
 
   // based on this Q&A: http://bit.ly/2kXgNGj
   if(process.env["DEBUG_FD"]) {
